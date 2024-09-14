b63b2a3d1e5cbedac3e0cab56b5fbf03d9a1a534
Erik Berns <erik.christian.berns@gmail.com>
2024-09-13 23:45:58 -0300

    a

--- ArchivosTP/src/impl/DiccionarioSimple.java
+++ ArchivosTP/src/impl/DiccionarioSimple.java
@@ -4,15 +4,13 @@ import api.DiccionarioSimpleTDA;
 import impl.Conjunto;
 
 public class DiccionarioSimple implements DiccionarioSimpleTDA {
-    // Clase interna para representar un elemento clave-valor
+
     class Elemento {
         int clave;
         int valor;
     }
 
-    // Arreglo para guardar los elementos
     Elemento[] elementos;
-    // Variable para guardar la cantidad de elementos en el diccionario
     int cant;
 
     public void InicializarDiccionario() {
@@ -31,7 +29,6 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
         elementos[pos].valor = valor;
     }
 
-    // Busca el índice de una clave en el arreglo de elementos
     private int Clave2Indice(int clave) {
         int i = cant - 1;
         while (i >= 0 && elementos[i].clave != clave) {
@@ -40,7 +37,6 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
         return i;
     }
 
-    // Elimina una clave (y su valor asociado) del diccionario
     public void Eliminar(String clave) {
         int pos = Clave2Indice(clave);
         if (pos != -1) {
@@ -49,7 +45,6 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
         }
     }
 
-    // Recupera el valor asociado a una clave
     public float Recuperar(String clave) {
         int pos = Clave2Indice(clave);
         if(pos != -1){
@@ -60,9 +55,8 @@ public class DiccionarioSimple implements DiccionarioSimpleTDA {
         }
     }
 
-    // Devuelve un conjunto de todas las claves presentes en el diccionario
     public ConjuntoTDA Claves() {
-        ConjuntoTDA c = new ConjuntoEstatico();
+        ConjuntoTDA c = new Conjunto();
         c.InicializarConjunto();
         for (int i = 0; i < cant; i++) {
             c.Agregar(elementos[i].clave);
