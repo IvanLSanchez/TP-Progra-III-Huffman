c796b70019a7cc519fecda9429f707136a337e15
Erik Berns <erik.christian.berns@gmail.com>
2024-09-13 22:52:51 -0300

    conjunto

---
+++ ArchivosTP/src/api/ConjuntoTDA.java
@@ -0,0 +1,12 @@
+package api;
+
+import impl.NodoHuffman;
+
+public interface ConjuntoTDA {
+    void InicializarConjunto();
+    boolean ConjuntoVacio();
+    void Agregar(int x);
+    int Elegir();
+    void Sacar(int x);
+    boolean Pertenece(int x);
+}
