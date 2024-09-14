05f52e819d95514871e5e87a9bcf1d38684d1683
Erik Berns <erik.christian.berns@gmail.com>
2024-09-13 22:59:10 -0300

    diccionario

---
+++ ArchivosTP/src/api/DiccionarioSimpleTDA.java
@@ -0,0 +1,11 @@
+package api;
+
+import impl.NodoHuffman;
+
+public interface DiccionarioSimpleTDA {
+    void InicializarDiccionario();
+    void Agregar(int clave, int valor);
+    void Eliminar(int clave);
+    int Recuperar(int clave);
+    ConjuntoTDA Claves();
+}
