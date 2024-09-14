9f013f1b9959e9dd098a85a285590c3c9349d98a
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2024-09-14 00:28:27 -0300

    Update metodosArbol.java

--- ArchivosTP/src/metodos/metodosArbol.java
+++ ArchivosTP/src/metodos/metodosArbol.java
@@ -1,27 +1,29 @@
 package metodos;
 
+import impl.NodoHuffman;
+
 public class metodosArbol {
-    public static void PreOrder(ABBTDA a){
-        if(!a.ArbolVacio()){
-            System.out.println(a.Raiz());
-            PreOrder(a.HIjoIzq());
-            PreOrder(a.HIjoDer());
+    public static void PreOrder(NodoHuffman a){
+        if(!(a.info==null)){
+            System.out.println(a.info);
+            PreOrder(a.hijoIzq);
+            PreOrder(a.hijoDer);
         }
     }
 
-    public static void InOrder(ABBTDA a){
-        if(!a.ArbolVacio()){
-            InOrder(a.HIjoIzq());
-            System.out.println(a.Raiz());
-            InOrder(a.HIjoDer());
+    public static void InOrder(NodoHuffman a){
+        if(!(a.info==null)){
+            InOrder(a.hijoIzq);
+            System.out.println(a.info);
+            InOrder(a.hijoDer);
         }
     }
 
-    public static void PostOrder(ABBTDA a){
-        if(!a.ArbolVacio()){
-            PostOrder(a.HIjoIzq());
-            PostOrder(a.HIjoDer());
-            System.out.println(a.Raiz());
+    public static void PostOrder(NodoHuffman a){
+        if(!(a.info==null)){
+            PostOrder(a.hijoIzq);
+            PostOrder(a.hijoDer);
+            System.out.println(a.info);
         }
     }
 }
