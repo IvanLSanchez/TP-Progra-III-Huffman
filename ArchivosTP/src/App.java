c0a72b94110e52e2f586fc9a337a2d3fead2eb6e
Ivan Lautaro Sanchez <100932334+IvanLSanchez@users.noreply.github.com>
2024-09-15 11:48:20 -0300

    actualizacion
    Se amplia el main para crear un arbol de huffman de todo el alfabeto
    Tambien se mueve el metodo de codificacion a Huffman y se lo renombra de manera mas representativa

--- ArchivosTP/src/App.java
+++ ArchivosTP/src/App.java
@@ -8,17 +8,40 @@ public class App {
     public static void main(String[] args) {
         DiccionarioSimpleTDA dic = new DiccionarioSimple();
         dic.InicializarDiccionario();
-        dic.Agregar("A", 45);
-        dic.Agregar("D", 16);
-        dic.Agregar("B", 13);
-        dic.Agregar("C", 12);
-        dic.Agregar("E", 9);
-        dic.Agregar("F", 5);
+        
+        dic.Agregar("A", 12.59f);
+        dic.Agregar("B", 1.44f);
+        dic.Agregar("C", 4.68f);
+        dic.Agregar("D", 5.81f);
+        dic.Agregar("E", 13.68f);
+        dic.Agregar("F", 0.69f);
+        dic.Agregar("G", 0.92f);
+        dic.Agregar("H", 0.73f);
+        dic.Agregar("I", 6.25f);
+        dic.Agregar("J", 0.44f);
+        dic.Agregar("K", 0.11f);
+        dic.Agregar("L", 5.46f);
+        dic.Agregar("M", 2.41f);
+        dic.Agregar("N", 7.97f);
+        dic.Agregar("Ñ", 0.2f);
+        dic.Agregar("O", 8.68f);
+        dic.Agregar("P", 2.25f);
+        dic.Agregar("Q", 0.88f);
+        dic.Agregar("R", 6.87f);
+        dic.Agregar("S", 7.93f);
+        dic.Agregar("T", 4.58f);
+        dic.Agregar("U", 2.78f);
+        dic.Agregar("V", 1.04f);
+        dic.Agregar("W", 0.05f);
+        dic.Agregar("X", 0.22f);
+        dic.Agregar("Y", 0.91f);
+        dic.Agregar("Z", 0.43f);
+
 
         NodoHuffman muestra = Huffman.crearArbol(dic);
         
         System.out.println("Codigos");
-        metodosArbol.Codificacion(muestra, "");
-
+        Huffman.ListarCodigo(muestra, "");
+        metodosArbol.InOrder(muestra);
         }
 }
--- ArchivosTP/src/metodos/Huffman.java
+++ ArchivosTP/src/metodos/Huffman.java
@@ -49,4 +49,25 @@ public class Huffman {
             
         return colaPrioridadProbabilidadMin.Primero();
     }
+
+    public static String ObtenerCodigoLetra(){
+
+    }
+
+    public static void ListarCodigo(NodoHuffman a, String codigo){
+        /* El siguiente metodo busca mostrar todos los codigos de un arbol de huffman
+         * El codigo requerira de los siguientes atributos: el arbol de trabajar y un string que contenera el estado
+         * actual del codigo (el mismo debera iniciar en "")
+         * El proceso para obtener los codigos sera el siguiente
+         * - entrara a la estructura y se fijara si el nodo raiz es una hoja.
+         *  - de serlo se imprimira el valor de la hoja junto con el codigo obtenido hasta el momento
+         *  - de no serlo le cambiara la raiz a sus hijos y agregara al codigo un 0 o un 1 dependiendo a que hijo fue.
+         */
+        if(a.hijoIzq.info==null && a.hijoDer.info==null){
+            System.out.println("- " + a.info + ": " + codigo);
+        }else{
+            Codificacion(a.hijoIzq, codigo + "0");
+            Codificacion(a.hijoDer, codigo + "1");
+        }
+    }
 }
